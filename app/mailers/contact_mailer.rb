da8ecaf72ce51f22e5399cebcbd56a5803674df8
Sainath Reddy <nsainath.bits@gmail.com>
2015-11-07 22:28:48 -0800

    Added sendgrid for mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'rob@simplecodecasts.com'
+  default to: 'nsainath.bits@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
